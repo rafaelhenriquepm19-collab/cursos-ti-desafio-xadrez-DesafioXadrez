74af3a73c972565ee5e240afa6ed1a853dd1d942
Rafael_xzl <rafaelhenriquepm19@gmail.com>
2026-03-10 01:43:43 +0000

    Loop aninhado com for e while

---
+++ XadrezAdvent.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+     
+    
+    for (int i = 0; i < 1; i++) {
+
+        int j = 0;
+
+        while(j < 2) {
+            printf(".Cavalo - baixo\n");
+            j++;  
+        } 
+        
+        printf(".Cavalo - esquerda\n");  
+    } 
+}
